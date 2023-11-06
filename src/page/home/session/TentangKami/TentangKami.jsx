2041e3b598a450f7f0ce201631031090692b25b4
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2023-11-06 12:35:49 +0700

    add: fake api

--- src/page/home/session/TentangKami/TentangKami.jsx
+++ src/page/home/session/TentangKami/TentangKami.jsx
@@ -1,88 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useState,useEffect } from "react";
+import axios from 'axios';
+import ChooseUs from "../../../../components/chooseUs/ChooseUs";
+import Partner from "../../../../components/partner/Partner";
 import './TentangKami.css';
 
 
+
 const TentangKami = () => {
+    const [data, setData] = useState({});
+    const apiTentangKami = 'http://localhost:2700/tentangKami'; // Use http://localhost
+    const getTentangKami = () => {
+        axios.get(apiTentangKami)
+        .then(response => {
+            setData(response.data[0]);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }
+    useEffect(() => {
+        getTentangKami()
+    }, []);
+    
     return (
         <Fragment>
             <div className="tentangKami-container">
                 <h1 className="title">TENTANG KAMI</h1>
                 <div className="top-container">
                     <img src="struktur.png" alt="" />
-                    <p>CV.TIMOER SENTOSA ( HERBAL YUNIARI ) berdiri sejak 24 September 2014. Perusahaan ini memproduksi aneka minuman herbal .Eksistensi dan komitmen kami membangun usaha ini terinspirasi dari minimnya kesadaran masyarakat sekitar akan potensi alam yang melimpah ,terutama hasil rempah – rempah. Oleh karena itu kami memberikan edukasi kepada masyarakat akan pentingnya pemanfaatan potensi alam tersebut untuk meningkatkan kesehatan dan Pendapatan guna meningkatkan kesejahteraan masyarakat. Ini kami wujudkan dengan membentuk komunitas jamu dikalurahan wareng yang sudah beranggotakan kurang lebih 100 ibu – ibu.</p>
+                    {/* <p>{data.tentang}</p> */}
                 </div>
                 <div className="mid-container">
                     <div className="left-contain">
                         <h1>VISI</h1>
-                        <p>Menjadi Perusahaan Minuman Herbal yang bermutu dan mampu memberikan alternatif serta solusi dalam upaya meningkatkan kualitas kesehatan masyarakat dengan menghasilkan produk-produk herbal alami yang berkualitas, terjangkau dan aman untuk dikonsumsi</p>
+                        {/* <p>{data.visi}</p> */}
                     </div>
                     <div className="right-contain">
                         <h1>MISI</h1>
                         <ol>
-                            <li>Menjadi perusahaan minuman herbal berkualitas dengan tetap melestarikan warisan herbal nusantara dalam meningkatkan kualitas kesehatan masyarakat. </li>
-                            <li>Meningkatkan pendapatan keluarga dengan pemanfaatan TOGA ( Tanaman Obat Keluarga ).</li>
-                            <li>Membangun perusahaan herbal yang memberikan nilai ekonomis kepada masyarakat sekitarnya.</li>
+                        {/* {Array.isArray(data.misi) && data.misi.map((item, index) => (
+                                <li id={item.id}>{item}</li>
+                            ))} */}
                         </ol>
                     </div>
                 </div>
-                <div className="bottom-container">
-                    <h1 className="title-bottom">KENAPA MEMILIH KAMI?</h1>
-                    <div className="card-wrapper">
-                        <div className="card">
-                            <img src="amico.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Bahan Baku Herbal Dari Masyarakat Dengan Kualitas Terbaik</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="choose2.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Produk Dengan 100% Bahan Herbal</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="choose3.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Diramu Oleh Tenaga Ahli Berpengalaman Puluhan Tahun di Bidang Herbal</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="choose4.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Diproses Higienis dibawah pengawasan profesional</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="choose5.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Dikemas Higienis dibawah pengawasan profesional</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="partner-container">
-                    <h1 className="title-partner">PARTNER KAMI</h1>
-                    <div className="card-wrapper">
-                        <div className="card">
-                            <img src="partner1.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Dinas Kelautan dan Perikanan Kabupaten Gunungkidul</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="partner2.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Dinas Perindustrian dan Perdagangan Kabupaten Gunungkidul</h1>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img src="partner2.png" alt=""/>
-                            <div className="card-title">
-                                <h1>Dinas Pertanian Kabupaten Gunungkidul</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <ChooseUs />
+                <Partner />
             </div>
         </Fragment>
     )
--- src/page/home/session/produk/Produk.jsx
+++ src/page/home/session/produk/Produk.jsx
@@ -1,4 +1,7 @@
 import React, { Fragment } from "react";
+import ProdukTerlaris from "../../../../components/ProdukTerlaris/ProdukTerlaris";
+import ProdukUnggulan from "../../../../components/produkUnggulan/ProdukUnggulan";
+import ProdukBundling from "../../../../components/produkBundling/ProdukBundling";
 import './Produk.css';
 
 
@@ -7,89 +10,11 @@ const Produk = () => {
         <Fragment>
             <div className="produk-container">
                 <div className="top-container">
-                    <div className="left-contain">
-                        <h1>PRODUK UNGGULAN KAMI</h1>
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <img src="produk1.png" alt=""/>
-                                <div className="card-title">
-                                    <h1>Wedang Uwuh</h1>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img src="produk2.png" alt=""/>
-                                <div className="card-title">
-                                    <h1>Jahe Merah Premium</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="right-contain">
-                        <h1>PRODUK TERLARIS BULAN INI</h1>
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <img src="produk3.png" alt=""/>
-                                <div className="card-title">
-                                    <h1>Wedang Uwuh</h1>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img src="produk4.png" alt=""/>
-                                <div className="card-title">
-                                    <h1>Telang Siap Minum</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ProdukUnggulan />
+                    <ProdukTerlaris />
                 </div>
                 <div className="bottom-container">
-                    <h1>PRODUK BUNDLING MINGGU INI</h1>
-                        <div className="card-wrapper">
-                                <div className="card">
-                                    <img src="bundling.png" alt=""/>
-                                    <div className="card-body">
-                                        <h1 className="card-title">Bundling Penurun Berat Badan</h1>
-                                        <ul className="card-text">
-                                            <li>Instan 150 gr</li>
-                                            <li>Fitslim Siap Minum</li>
-                                        </ul>
-                                        <button>Hubungi Admin</button>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <img src="bundling.png" alt=""/>
-                                    <div className="card-body">
-                                        <h1 className="card-title">Bundling Perawatan Kulit</h1>
-                                        <ul className="card-text">
-                                            <li>Instan 150 gr</li>
-                                            <li>Kapsul Kulit Manggis</li>
-                                        </ul>
-                                        <button>Hubungi Admin</button>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <img src="bundling.png" alt=""/>
-                                    <div className="card-body">
-                                        <h1 className="card-title">Bundling Peningkat Imun Tubuh</h1>
-                                        <ul className="card-text">
-                                            <li>Wedang Telang</li>
-                                            <li>Wedang Pitu</li>
-                                        </ul>
-                                        <button>Hubungi Admin</button>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <img src="bundling.png" alt=""/>
-                                    <div className="card-body">
-                                        <h1 className="card-title">Bundling Perawatan Asam Urat</h1>
-                                        <ul className="card-text">
-                                            <li>Kapsul Amufit</li>
-                                            <li>Jamu Godog</li>
-                                        </ul>
-                                        <button>Hubungi Admin</button>
-                                    </div>
-                                </div>
-                        </div>
+                    <ProdukBundling />
                 </div>
             </div>
 
